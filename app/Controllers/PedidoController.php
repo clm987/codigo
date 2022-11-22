7a4fcfad65a66512709ea579971ab96cf4cd80f3
Carlos <clm987@gmail.com>
2022-11-22 19:14:31 -0300

    descomento codigo

--- app/Controllers/PedidoController.php
+++ app/Controllers/PedidoController.php
@@ -9,31 +9,31 @@ class PedidoController extends Pedido
 {
     public function CargarUno($request, $response, $args)
     {
-        // $parametros = $request->getParsedBody();
-        // $id_mesa = $parametros['id_mesa'];
-        // $nombre_cliente = $parametros['nombre_cliente'];
-        // $listaProductos = $parametros['lista_productos'];
-        // $id_mozo = $request->getHeaderLine('idMozo');
+        $parametros = $request->getParsedBody();
+        $id_mesa = $parametros['id_mesa'];
+        $nombre_cliente = $parametros['nombre_cliente'];
+        $listaProductos = $parametros['lista_productos'];
+        $id_mozo = $request->getHeaderLine('idMozo');
 
-        // // Creamos el Pedido
-        // $auxPedido = new Pedido();
-        // $auxPedido->estado = EEPedido::PENDIENTE;
-        // $auxPedido->id_mesa = $id_mesa;
-        // $auxPedido->nombre_cliente = $nombre_cliente;
-        // $auxPedido->id_mozo = $id_mozo;
-        // $nuevoId = $auxPedido->crearPedido();
-        // if ($nuevoId > 0) {
-        //     if (PedidoProducto::cargarLista($listaProductos, $nuevoId)) {
-        //         $payload = json_encode(array("mensaje" => "Pedido creado con exito", "Codigo_Pedido: " => $auxPedido->codigo_pedido));
-        //         $response->getBody()->write($payload);
-        //     } else {
-        //         $payload = json_encode(array("mensaje" => "Ocurrio un error."));
-        //         $response->getBody()->write($payload);
-        //     }
-        // } else {
-        //     $payload = json_encode(array("mensaje" => "Ocurrio un error."));
-        //     $response->getBody()->write($payload);
-        // }
+        // Creamos el Pedido
+        $auxPedido = new Pedido();
+        $auxPedido->estado = EEPedido::PENDIENTE;
+        $auxPedido->id_mesa = $id_mesa;
+        $auxPedido->nombre_cliente = $nombre_cliente;
+        $auxPedido->id_mozo = $id_mozo;
+        $nuevoId = $auxPedido->crearPedido();
+        if ($nuevoId > 0) {
+            if (PedidoProducto::cargarLista($listaProductos, $nuevoId)) {
+                $payload = json_encode(array("mensaje" => "Pedido creado con exito", "Codigo_Pedido: " => $auxPedido->codigo_pedido));
+                $response->getBody()->write($payload);
+            } else {
+                $payload = json_encode(array("mensaje" => "Ocurrio un error."));
+                $response->getBody()->write($payload);
+            }
+        } else {
+            $payload = json_encode(array("mensaje" => "Ocurrio un error."));
+            $response->getBody()->write($payload);
+        }
         $payload = json_encode(array("mensaje" => "prueba."));
         $response->getBody()->write($payload);
         return $response
